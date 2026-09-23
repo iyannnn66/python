b5ff30a41fbbe692ac5596290d2222102ceff525
iyannnn66 <satrioharimurti.06@gmail.com>
2026-09-23 15:28:47 +0800

    feat : add Pembelajaran Python W3Shcools

---
+++ Python Operators/Membership Operators.py
@@ -0,0 +1,7 @@
+fruits = ["apple", "banana", "cherry"]
+
+print("banana" in fruits)
+
+fruits = ["apple", "banana", "cherry"]
+
+print("pineapple" not in fruits)
---
+++ Python Operators/Python Lists/List Items - Data Types.py
@@ -0,0 +1,5 @@
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+list1 = ["abc", 34, True, 40, "male"]
